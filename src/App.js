990a8f2df77e8eb1c0679fd70fc4d379fb773787
nishchala <28265373+nishchala@users.noreply.github.com>
2022-05-04 22:03:17 -0400

    Header component added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hello World</h1>
-      </header>
+      <Header />
     </div>
   );
 }
